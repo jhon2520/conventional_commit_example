357ed52245b253ba27f3c65db0c85938150e8ec2
jhon2520 <63693418+jhon2520@users.noreply.github.com>
2024-06-16 15:51:06 -0500

    fix: prueba
    prueba

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+      home: _HomeScreen(),
+    );
+  }
+}
+
+class _HomeScreen extends StatelessWidget {
+  const _HomeScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Material App Bar'),
+      ),
+      body: const Center(
+        child: Text('cambio nombre'),
       ),
     );
   }
